501634f83f171b8c64fe4f25d4b13176088f0941
Isaac Pak <isaac.pak@adxsoftware.com>
2022-01-17 22:16:54 -0600

    init

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+function relativeToPackage(configPath) {
+  return path.resolve(
+    process.cwd(),
+    process.env.npm_package_repository_directory,
+    configPath
+  );
+}
+
+module.exports = {
+  setupFilesAfterEnv: ["<rootDir>/jest/custom-matchers.ts"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+      tsConfig: relativeToPackage("./tsconfig.tsdx.json"),
+    },
+  },
+};
